4cdc08408659a70392d746c861374c641259e6bc
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-06-09 10:25:43 +0300

    Code refactoring.

--- src/main/java/com/salimov/yurii/Main.java
+++ src/main/java/com/salimov/yurii/Main.java
@@ -3,17 +3,25 @@ package com.salimov.yurii;
 import com.salimov.yurii.encryptor.Base64Encryptor;
 import com.salimov.yurii.encryptor.Encryptor;
 
+import java.security.NoSuchAlgorithmException;
+
 /**
  * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
  * @version 1.0
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         final Encryptor encryptor = new Base64Encryptor();
+
         final String login = "admin";
+        final String enLogin = encryptor.encrypt(login);
+        final String decLogin = encryptor.decrypt(enLogin);
+        System.out.println(login + " - " + enLogin + " - " + decLogin);
+
         final String password = "password";
-        System.out.println(encryptor.encrypt(login));
-        System.out.println(encryptor.encrypt(password));
+        final String enPass = encryptor.encrypt(password);
+        final String decPass = encryptor.decrypt(enPass);
+        System.out.println(password + " - " + enPass + " - " + decPass);
     }
 }
